6362eb2c93d5484702412facf12c3bb7ffe9bf39
Maria Cecilia Caruzzo Modica <ceciliacmodica@gmail.com>
2025-05-08 16:38:06 -0300

    Alterações dos nomes de variáveis

--- src/login/login.js
+++ src/login/login.js
@@ -3,6 +3,7 @@
 const formLogin = document.getElementById("form-login");
 const inputPassword = document.getElementById("form-password");
 const inputEmail = document.getElementById("form-email");
+const formInputMessage = document.getElementById("form-input-message");
 
 //Abaixo é a parte de quando o formulário for enviado. Significa: “Quando o usuário tentar enviar o formulário, execute essa função” (event.preventDefault() previne o recarregamento da página, o refresh)
 formLogin.addEventListener("submit", function (event) {
@@ -13,22 +14,22 @@ formLogin.addEventListener("submit", function (event) {
     inputPassword.value == "123456"
   ) {
     //Entra nesse bloco ({}) caso os dados inseridos em e-mail e senha forem corretos, ou seja, o e-mail digitado = "ceciliacmodica@gmail.com" e senha = 123456
-    document.getElementById("form-input-message").style.display = "none"; //Faz sumir a mensagem de erro (display = "none") enquanto os dados forem colocados forem corretos
+    formInputMessage.style.display = "none"; //Faz sumir a mensagem de erro (display = "none") enquanto os dados forem colocados forem corretos
     window.location.href = "/src/Main/Main.html"; //Redireciona o usuário para outra página (main)
   } else {
     //Entra nesse bloco ({}) caso os dados inseridos em e-mail e senha forem incorretos, ou seja, o e-mail digitado != "ceciliacmodica@gmail.com" e senha != 123456
-    document.getElementById("form-input-message").style.display = "block"; //Mostra a mensagem de erro (display = "block"), que estava "escondida" por padrão
+    formInputMessage.style.display = "block"; //Mostra a mensagem de erro (display = "block"), que estava "escondida" por padrão
   }
 });
 
 inputEmail.addEventListener("input", function () {
   //.addEventListener adiciona um ouvinte para o elemento HTML (form, input...) quando um evento do tipo input for disparado, ou seja, quando o usuário começar a digitar no campo e-mail, a função sera executada
-  document.getElementById("form-input-message").style.display = "none"; //Isso faz com que a mensagem de erro suma (display = none)
+  formInputMessage.style.display = "none"; //Isso faz com que a mensagem de erro suma (display = none)
 });
 
 inputPassword.addEventListener("input", function () {
   //.addEventListener adiciona um ouvinte para o elemento HTML (form, input...) quando um evento do tipo input for disparado, ou seja, quando o usuário começar a digitar no campo senha, a função sera executada
-  document.getElementById("form-input-message").style.display = "none"; //Isso faz com que a mensagem de erro suma (display = none)
+  formInputMessage.style.display = "none"; //Isso faz com que a mensagem de erro suma (display = none)
 });
 
 /*const form = document.getElementById("form-contato");
