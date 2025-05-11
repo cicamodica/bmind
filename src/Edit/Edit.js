1f2472197dac2292587056b4866468e73f02b95d
Diogo Liberato Jóia Mariano <136910283+diogojoia777@users.noreply.github.com>
2025-05-10 21:51:40 -0300

    Create Edit.js

---
+++ src/Edit/Edit.js
@@ -0,0 +1,14 @@
+//Recupera o usuario logado
+const usuarioLogado = localStorage.getItem('currentUser');
+
+// Verifica se o usuário está logado
+//if (usuarioLogado) {
+   //Se o usuário estiver logado, exibe uma mensagem de boas-vindas
+  //const usuario = JSON.parse(usuarioLogado);
+  //const boasVindas = document.getElementById('boasVindas');
+  //boasVindas.innerHTML = `Bem-vindo(a), ${usuario.nome}!`;
+//}
+// Caso contrário, redireciona para a página de login
+//else {
+  //window.location.href = 'login.html';
+//}
