66979dc20e7366a974f3be3772a41b448d489145
Maria Cecilia Caruzzo Modica <ceciliacmodica@gmail.com>
2025-05-21 12:11:11 -0300

    Adicionando menu na tela de edição de dashboard + funcionalidade na barra de pesquisa + fonte Poppins + ícone de perfil com link

--- src/Tela de Edição/script_Tela_de_Edição.js
+++ src/Tela de Edição/script_Tela_de_Edição.js
@@ -1,3 +1,35 @@
+//Funcionalidade da pesquisa (barra de pesquisa) > lê na URL o que foi pesquisado e procura nos conteúdos
+document
+  .getElementById("search-button")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // evita o redirecionamento padrão
+    const termo = document.getElementById("search-bar").value.trim();
+    if (termo !== "") {
+      const encodedTermo = encodeURIComponent(termo);
+      window.location.href = `/src/resultado-de-pesquisa/logado/logado-pf/resultado-de-pesquisa-pf.html?q=${encodedTermo}`;
+    }
+  });
+
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  const navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.toggle("hidden");
+});
+
+function toggleMenu() {
+  // Função para alternar o menu dropdown
+  const menu = document.getElementById("dropdownMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+// Fechar menu ao clicar fora
+window.addEventListener("click", function (e) {
+  const menu = document.getElementById("dropdownMenu");
+  const icon = document.querySelector(".menu-icon");
+  if (!menu.contains(e.target) && !icon.contains(e.target)) {
+    menu.style.display = "none";
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     // =================== ICONE DE VOLTAR ===================
     const voltar = document.getElementById("botaovoltar");
