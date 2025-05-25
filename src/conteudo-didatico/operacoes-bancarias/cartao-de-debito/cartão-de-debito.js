c60abb2e5a3d613f417e6b679218d518e286f5e4
Mariana Gressi <marianatgressi@gmail.com>
2025-05-25 01:44:32 -0300

    Add menu reestruturado com links e função de atualizar a inteface conforme perfil logado em todas as paginas de conteudo

--- src/conteudo-didatico/operacoes-bancarias/cartao-de-debito/cartão-de-debito.js
+++ src/conteudo-didatico/operacoes-bancarias/cartao-de-debito/cartão-de-debito.js
@@ -1,3 +1,51 @@
+
+function atualizarInterfaceUsuario() {
+  const usuarioLogado = localStorage.getItem("usuarioLogado");
+  const dadosDoUsuario = usuarioLogado ? JSON.parse(localStorage.getItem(usuarioLogado)) : null;
+
+  const userActionsLogado = document.getElementById("perfil-logado");
+  const userActionsNaoLogado = document.getElementById("login-nao-logado");
+
+  const itensPF = document.querySelectorAll('.item-pf');
+  const itensPJ = document.querySelectorAll('.item-pj');
+  const itensLogado = document.querySelectorAll('.item-logged');
+  const itensNaoLogado = document.querySelectorAll('.item-nao-logado');
+
+  const estaNaPaginaMinhaArea = window.location.pathname.includes("/src/Main/Main.html")
+
+  // Esconde tudo inicialmente
+  userActionsLogado.style.display = "none";
+  userActionsNaoLogado.style.display = "none";
+  itensPF.forEach(el => el.style.display = 'none');
+  itensPJ.forEach(el => el.style.display = 'none');
+  itensLogado.forEach(el => el.style.display = 'none');
+  itensNaoLogado.forEach(el => el.style.display = 'none');
+
+  if (dadosDoUsuario && (dadosDoUsuario.perfil === "Pessoa Física" || dadosDoUsuario.perfil === "Pessoa Jurídica")) {
+
+    
+    // Só mostra o botão "perfil-logado" se não estiver na própria página de "Minha Área"
+    if (!estaNaPaginaMinhaArea && userActionsLogado) {
+      userActionsLogado.style.display = "flex";
+    }
+
+    // Exibe itens de logado
+    itensLogado.forEach(el => el.style.display = 'block');
+
+    if (dadosDoUsuario.perfil === "Pessoa Física") {
+      itensPF.forEach(el => el.style.display = 'block');
+    } else if (dadosDoUsuario.perfil === "Pessoa Jurídica") {
+      itensPJ.forEach(el => el.style.display = 'block');
+    }
+  } else {
+    // Usuário não logado ou com dados inválidos
+    userActionsNaoLogado.style.display = "flex";
+    itensNaoLogado.forEach(el => el.style.display = 'block');
+  }
+}
+
+// Executa quando a página terminar de carregar
+document.addEventListener('DOMContentLoaded', atualizarInterfaceUsuario);
 //Funcionalidade da pesquisa (barra de pesquisa) > lê na URL o que foi pesquisado e procura nos conteúdos
 document
   .getElementById("search-button")
